85081cd56a226b6ad309f5f6c50aab5859dee274
Nazarena Garcia <nazagarcia132@gmail.com>
2024-09-26 14:48:58 -0300

    roles de usuarios

---
+++ frontend/src/routes/AppRouter.jsx
@@ -0,0 +1,34 @@
+// src/routes/AppRouter.jsx
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../views/login/Login";
+import Register from "../views/register/Register";
+import PrivateRoutes from "./PrivateRoutes";
+import PublicRoutes from "./PublicRoutes";
+import Estudiantes from "../views/estudiantes/Estudiantes"; 
+import Profesores from "../views/profesores/Profesores"; 
+import Admin from "../views/admin/Admin"; 
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rutas públicas accesibles solo si no está autenticado */}
+        <Route element={<PublicRoutes />}>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        {/* Rutas privadas accesibles solo si está autenticado */}
+        <Route element={<PrivateRoutes />}>
+          <Route path="/estudiantes" element={<Estudiantes />} />
+          <Route path="/profesores" element={<Profesores />} />
+          <Route path="/admin" element={<Admin />} />
+      
+      
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
---
+++ frontend/src/views/admin/admin.jsx
@@ -0,0 +1,36 @@
+// src/views/Estudiantes.jsx
+import React, { useContext } from 'react';
+import Navbar from './navbar';
+import Banner from './banner';
+import Categories from './categories';
+import { UserContext } from '../../context/UserContext'; // Importa el contexto
+import { userType } from '../../context/userTypes'; // Importa los tipos de acción
+
+function Admin() {
+  const { stateDispatch } = useContext(UserContext); // Accede al dispatch
+
+  const logOut = () => {
+    // Limpia el localStorage antes de la actualización de estado
+    localStorage.removeItem("userData");
+
+    // Actualiza el estado para reflejar el cierre de sesión
+    stateDispatch({
+      type: userType.logOut,
+    });
+
+    // Si es necesario, redirige al usuario a la pantalla de login
+    window.location.href = "/"; // O usa navigate("/")
+  };
+ 
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Banner />
+      <Categories />
+      <button onClick={logOut}>Cerrar Sesión (ad)</button>
+    </div>
+  );
+}
+
+export default Admin;
